e6394ade9c699151305054ee2bf6741bb079b686
Rodrigo Andre Gutierrez <rodrigoandregutierrez@gmail.com>
2023-06-12 04:45:31 -0400

    add html structure and styles

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['estandard', 'eslint-config-prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+}
